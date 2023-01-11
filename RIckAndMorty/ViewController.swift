4e3dc117fa082845977bb144293f6d675ed99762
IlkinIOS <ilkinmurtuzayevv@gmail.com>
2023-01-11 22:29:14 +0400

    Initial Commit

---
+++ RIckAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIckAndMorty
+//
+//  Created by Ilkin Murtuzayev on 11.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
